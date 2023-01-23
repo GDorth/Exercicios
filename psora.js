6373257a2f7e1f38b00bf1cbd887b10adee2331a
GDorth <116523001+GDorth@users.noreply.github.com>
2023-01-23 20:10:28 -0300

    Correção id="menu"

--- psora.js
+++ psora.js
@@ -1,6 +1,6 @@
 $(document).ready(function () { 
 	$(".mn").click(function(){
-		$(".submn1").slideUp ("slow");
+		$(".submn1").slideDown ("slow");
 		$(this).next ("submn1").slideDown("slow");
 	})
  });
