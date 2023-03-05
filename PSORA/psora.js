53be982aa356bf2f4f4c09c59c0b1b9a93dc0fd2
GDorth <116523001+GDorth@users.noreply.github.com>
2023-03-04 21:48:13 -0300

    Alterações para estudo

---
+++ PSORA/psora.js
@@ -0,0 +1,12 @@
+$ (document).ready.do {
+	(
+} while (condition);function){
+
+ $( function() {
+    $( "#accordion" ).accordion();
+  } );
+
+	$(".mn").click(function(){
+		$(".submn1").slideUp ("slow");
+		$(this).next ("submn1").slideDown("slow");
+	})
